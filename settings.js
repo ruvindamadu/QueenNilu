1f5d197e98b331410a01329a306433ab61f28c1f
ruvindamadu <131520842+ruvindamadu@users.noreply.github.com>
2023-04-22 15:02:19 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,8 +7,8 @@ const chalk = require('chalk')
 */
 
 //Owner Number & Name 
-global.owner = ['94767438882'] //ur owner number
-global.ownername = "Janith sadanuwan" //ur owner name
+global.owner = ['94702650071'] //ur owner number
+global.ownername = "Ruvinda Madushan" //ur owner name
 
 //Bot Name 
 global.botname = 'ǫᴜᴇᴇɴ ɴɪʟᴜ' //ur bot name
